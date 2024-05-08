aab5913f186cfc615e09158dff5d923378c7b4d8
Christian Raue <captain_2@gmx.de>
2024-05-08 13:13:35 +0200

    implemented audio lib driver program

---
+++ src/main.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "audio.h"
+
+void mainloop(AudioObject *audio) {
+    pthread_barrier_t barrier;
+    pthread_barrier_init(&barrier, NULL, 1);
+
+    audioPlay(audio, &barrier);
+    AudioError *error = audioGetError(audio);
+    printf("Playing\n");
+    if (error->level == AUDIO_ERROR_LEVEL_WARNING) {
+        const char *errorString = audioGetErrorString(error);
+        audioResetError(audio);
+        fprintf(stderr, "Warning: %s\n", errorString);
+    }
+
+    while (true) {
+        switch (getchar()) {
+            case 'p':
+                audioPause(audio, &barrier);
+                printf("Paused\n");
+                break;
+
+            case 'r':
+                audioPlay(audio, &barrier);
+                printf("Resumed\n");
+                break;
+
+            case 's':
+                audioStop(audio, &barrier);
+                printf("Stopped\n");
+                break;
+
+            case 'j':
+                uint32_t milliseconds;
+                if (scanf("%u", &milliseconds) == EOF) {
+                    fprintf(stderr, "Could not read time.");
+                    break;
+                }
+                audioJump(audio, &barrier, milliseconds);
+                printf("Jumped to %u milliseconds\n", milliseconds);
+                break;
+
+            case 't':
+                uint32_t currentTime = audioGetCurrentTime(audio);
+                float currentTimeSeconds = currentTime / 1000.0f;
+                printf("Current time: %.2f seconds\n", currentTimeSeconds);
+                break;
+
+            case 'q':
+                printf("Quitting\n");
+                return;
+        }
+        if (error->level == AUDIO_ERROR_LEVEL_WARNING) {
+            const char *errorString = audioGetErrorString(error);
+            audioResetError(audio);
+            fprintf(stderr, "Warning: %s\n", errorString);
+        }
+    }
+
+    audioDestroy(audio);
+    pthread_barrier_destroy(&barrier);
+}
+
+void * readFile(FILE *file, size_t fileSize) {
+    void *rawData = malloc(fileSize);
+    if (rawData == NULL) {
+        perror("malloc");
+        fclose(file);
+        exit( EXIT_FAILURE);
+    }
+
+    if (fread(rawData, 1, fileSize, file) != fileSize) {
+        perror("fread");
+        free(rawData);
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    return rawData;
+}
+
+void * mapFile(int fileDescriptor,  size_t fileSize) {
+    void *rawData = mmap(
+        NULL, fileSize, PROT_READ, MAP_PRIVATE, fileDescriptor, 0
+    );
+    if (rawData == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+    return rawData;
+}
+
+void printUsage(char *programName) {
+    fprintf(stderr, "Usage: %s [-m | -h] <WAV file>\n", programName);
+}
+
+void printHelp(char *programName) {
+    printUsage(programName);
+    putchar('\n');
+
+    printf("-m\t\tMap the file to memory instead of reading it.\n");
+    printf("-h\t\tShow this help.\n");
+    putchar('\n');
+
+    printf("After program start type the following commands to control palyback:\n");
+    putchar('\n');
+
+    printf("p\t\tPause playback.\n");
+    printf("r\t\tResume/start playback.\n");
+    printf("s\t\tStop playback.\n");
+    printf("j T\t\tJump to T milliseconds.\n");
+    printf("t\t\tShow current milliseconds.\n");
+    printf("q\t\tQuit program.\n");
+}
+
+void parseArguments(int argc, char *argv[], char **filename, bool *map, bool *showHelp) {
+    switch (argc) {
+        case 2:
+            if (!strncmp(argv[1], "-m", strnlen(argv[1], 3))) {
+                *showHelp = true;
+                return;
+            } else {
+                *filename = argv[1];
+            }
+            break;
+
+        case 3:
+            if (!strncmp(argv[1], "-m", strnlen(argv[1], 3))) {
+                *filename = argv[2];
+                *map = true;
+            } else if (!strncmp(argv[1], "-h", strnlen(argv[1], 3))) {
+                *showHelp = true;
+                return;
+            } else {
+                printUsage(argv[0]);
+                exit(EXIT_FAILURE);
+            }
+            break;
+
+        default:
+            printUsage(argv[0]);
+            exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *filename = NULL;
+    bool map = false;
+    bool showHelp = false;
+    parseArguments(argc, argv, &filename, &map, &showHelp);
+
+    if (showHelp) {
+        printHelp(argv[0]);
+        return EXIT_SUCCESS;
+    }
+
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    int fileDescriptor = fileno(file);
+    if (fileDescriptor == -1) {
+        perror("fileno");
+        exit(EXIT_FAILURE);
+    }
+
+    struct stat fileStats;
+    if (fstat(fileDescriptor, &fileStats) == -1) {
+        perror("fstat");
+        return EXIT_FAILURE;
+    }
+    size_t fileSize = fileStats.st_size;
+
+    void *rawData;
+    if (map) {
+        printf("Mapping file.\n");
+        rawData = mapFile(fileDescriptor, fileSize);
+    } else {
+        printf("Reading file.\n");
+        rawData = readFile(file, fileSize);
+        fclose(file);
+    }
+
+    AudioConfiguration configuration = {
+        .rawData = rawData,
+        .rawDataSize = fileSize,
+        .soundDeviceName = "default",
+        .soundDeviceNameSize = 8,
+        .timeResolution = 10
+    };
+
+    AudioObject *audio = audioInit(&configuration);
+    if (audio == NULL) {
+        fprintf(stderr, "Failed to initialize audio\n");
+        free(rawData);
+        return EXIT_FAILURE;
+    }
+    AudioError *error = audioGetError(audio);
+    if (error->level == AUDIO_ERROR_LEVEL_ERROR) {
+        const char *errorString = audioGetErrorString(error);
+        fprintf(stderr, "Error: %s\n", errorString);
+        free(rawData);
+        return EXIT_FAILURE;
+    }
+
+    uint32_t totalDuration = audioGetTotalDuration(audio);
+    float totalDurationSeconds = totalDuration / 1000.0f;
+    printf("Total duration: %.2f seconds\n", totalDurationSeconds);
+
+    mainloop(audio);
+
+    if (map) {
+        if (munmap(rawData, fileSize) == -1) {
+            perror("munmap");
+            return EXIT_FAILURE;
+        }
+        if (fclose(file)) {
+            perror("fclose");
+            return EXIT_FAILURE;
+        }
+    } else {
+        free(rawData);
+    }
+    return EXIT_SUCCESS;
+}
